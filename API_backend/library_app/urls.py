9824e4709c3b9b274ce908e192860968f19d575e
Noelbiggs28 <noelbiggs28@gmail.com>
2023-10-31 15:32:13 -1000

    added subjects

--- API_backend/library_app/urls.py
+++ API_backend/library_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('title/', views.TitleView.as_view()),
     path('author/', views.AuthorView.as_view()),
+    path('subject/', views.SubjectView.as_view()),
 
 ]
